5bd1bfc6e00758d834bca46022c7ef6f15d76a61
Alfredo Gutemberg <alfredo.gutemberg@gmail.com>
2022-05-26 21:53:51 -0300

    class Client

--- Javascript/JavascriptPOO/index.js
+++ Javascript/JavascriptPOO/index.js
@@ -1,15 +1,31 @@
+class Client {
+  name;
+  cpf;
+  agencia;
+  conta;
+  saldo;
+}
+
+const client1 = new Client(),
+  client2 = new Client(),
+  client3 = new Client();
+
 // Dados iniciais para abertura da conta
-const client1Name = "Pereira";
-const client1Cpf = 58788687090;
-const client1Agencia = 1001;
-const client1Saldo = 0;
+client1.name = "Pereira";
+client1.cpf = 58788687090;
+client1.agencia = 1001;
+client1.saldo = 0;
 
-const client2Name = "Júlia";
-const client2Cpf = 47860587009;
-const client2Agencia = 1001;
-const client2Saldo = 0;
+client2.name = "Júlia";
+client2.cpf = 47860587009;
+client2.agencia = 1001;
+client2.saldo = 0;
 
-const client3Name = "Ania";
-const client3Cpf = 92661493069;
-const client3Agencia = 1001;
-const client3Saldo = 0;
+client3.name = "Ania";
+client3.cpf = 92661493069;
+client3.agencia = 1001;
+client3.saldo = 0;
+
+console.log(client1);
+console.log(client2);
+console.log(client3);
