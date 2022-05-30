48da6e5c210cedd90d4957c1b76932180d2b64fd
Alfredo Gutemberg <alfredo.gutemberg@gmail.com>
2022-05-29 23:46:17 -0300

    ContaCorrent.js

---
+++ Javascript/JavascriptPOO/ContaCorrente.js
@@ -0,0 +1,27 @@
+class ContaCorrente {
+    agencia;
+    // A convenção adota underline (_) à frente de um atributo, isso significa que ele é privado
+    _saldo = 0;
+    // #saldo = 0 https://github.com/tc39/proposal-class-fields#private-fields
+  
+    // Método sacar
+    sacar(valor) {
+      if (this._saldo >= valor) {
+        this._saldo -= valor;
+        return valor;
+      }
+    }
+  
+    // Método depositar
+    // depositar(valor) {
+    //   if (valor > 0) {
+    //     this._saldo += valor;
+    //     console.log(this._saldo);
+    //   }
+    // }
+    depositar(valor) {
+      if (valor <= 0) return;
+  
+      this._saldo += valor;
+    }
+  }
