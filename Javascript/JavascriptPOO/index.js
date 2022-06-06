6b4f5096eae40620b43e7d61ec917f8b78c82476
Alfredo Gutemberg <alfredo.gutemberg@gmail.com>
2022-06-05 22:11:20 -0300

    cliente03 e conta03

--- Javascript/JavascriptPOO/index.js
+++ Javascript/JavascriptPOO/index.js
@@ -4,9 +4,10 @@ import {ContaCorrente} from "./ContaCorrente.js";
 //Aqui eu crio um cliente
 const cliente01 = new Cliente();
 const cliente02 = new Cliente();
+const cliente03 = new Cliente();
 
 
-// Dados iniciais para abertura da conta
+//Atribuição de dados ao cliente
 cliente01.firstName = "Pereira";
 cliente01.lastName = "da Silva";
 cliente01.cpf = 58788687090;
@@ -15,10 +16,27 @@ cliente02.firstName = "Alice";
 cliente02.lastName = "Gutierrez";
 cliente02.cpf = 81250709075;
 
+cliente03.firstName = "Thomas";
+cliente03.lastName = "Brady";
+cliente03.cpf = 48948962060;
 
+
+//Criação da conta
 const conta01 = new ContaCorrente();
+const conta02 = new ContaCorrente();
+const conta03 = new ContaCorrente();
+
+//Atribuindo Agência à conta
 conta01.agencia = 1001;
-conta01.cliente = cliente01
+conta02.agencia = 1002;
+conta03.agencia = 1003;
+
+//Vinculando um cliente a uma conta
+conta01.cliente = cliente01;
+conta02.cliente = cliente02;
+conta03.cliente = cliente03;
+
+
 
 // Ações feitas na conta do Pereira
 console.log(`Cliente: ${cliente01.firstName}`);
@@ -36,14 +54,9 @@ console.log(`Saldo: R$ ${conta01._saldo}`);
 
 console.log(conta01);
 
-//Criando uma conta corrente de nome "conta02"
-const conta02 = new ContaCorrente();
 
-// vinculando a conta "conta02" com o cliente "cliene02"
-conta02.cliente = cliente02;
 
-//aqui eu atribuo à agência um número de identificação
-conta02.agencia = 1010;
+
 
 console.log(conta02);
 
