031b5d4ed36f2523693a34229abecdd0088b14f7
Alfredo Gutemberg <alfredo.gutemberg@gmail.com>
2022-06-28 23:08:02 -0300

    contaCorrente = new

--- Javascript/ByteBank/index.js
+++ Javascript/ByteBank/index.js
@@ -8,20 +8,18 @@ class ContaCorrente {
     saldo;
 }
 
-
 const cliente1 = new Cliente();
-const cliente2 = new Cliente();
-
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233344;
-contaCorrente1.agencia = 1001;
-contaCorrente1.saldo = 0;
 
+const cliente2 = new Cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = 22233344455;
-// cliente2.agencia = 1001;
-// cliente2.saldo = 0;
 
-const contaCorrente1 = new ContaCorrente;
+
+const contaCorrenteRicardo = new ContaCorrente();
+
+console.log(cliente1);
+console.log(cliente2);
 
 
