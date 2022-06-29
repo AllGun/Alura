197f22a4d55a29c8ec34d2c3becb9d6c0ccfc4d7
Alfredo Gutemberg <alfredo.gutemberg@gmail.com>
2022-06-28 21:27:06 -0300

    class Cliente e ContaCorrente done

---
+++ Javascript/ByteBank/index.js
@@ -0,0 +1,27 @@
+class Cliente { // Criando uma classe
+    nome; //Entre as chaves eu tenho campos ou atributos
+    cpf;
+}
+
+class ContaCorrente {
+    agencia;
+    saldo;
+}
+
+
+const cliente1 = new Cliente();
+const cliente2 = new Cliente();
+
+cliente1.nome = "Ricardo";
+cliente1.cpf = 11122233344;
+contaCorrente1.agencia = 1001;
+contaCorrente1.saldo = 0;
+
+cliente2.nome = "Alice";
+cliente2.cpf = 22233344455;
+// cliente2.agencia = 1001;
+// cliente2.saldo = 0;
+
+const contaCorrente1 = new ContaCorrente;
+
+
